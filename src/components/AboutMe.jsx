048ad0883fbc1116a337a6e8acdf40cc599bd6ad
troskan <str4ndb3rg@live.se>
2023-04-06 12:51:47 +0100

    added more responsive design

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import ProfilePic from "../assets/profile-pic.png";
 function AboutMe() {
   return (
-    <section>
+    <section className="about-me-section">
       <h2 id="aboutme">About me</h2>
-      <p className="about-me">
-        My name is Alvin and I'm 27 years old. I have been working as a
-        carpenter for 7 years and have just switched career to programming which
-        I totally <strong>love!</strong> <br />
-        <br /> If I were to describe myself I'd say I'm a very happy and
-        positive person, but if you really would like to know more about me dont
-        hesitate to contact me!{" "}
-      </p>
-      <h2 id="footer-mail">alvin.strandberg@proton.me</h2>
-      <img id="profile-pic" src={ProfilePic} alt="" />
+      <div className="text-pic">
+        <p className="about-me">
+          My name is Alvin and I'm 27 years old. I have been working as a
+          carpenter for 7 years and have just switched career to programming
+          which I totally <strong>love!</strong> <br />
+          <br /> If I were to describe myself I'd say I'm a very happy and
+          positive person, but if you really would like to know more about me
+          dont hesitate to contact me!{" "}
+          <h2 id="footer-mail">alvin.strandberg@proton.me</h2>
+        </p>
+        <img id="profile-pic" src={ProfilePic} alt="" />
+      </div>
     </section>
   );
 }
