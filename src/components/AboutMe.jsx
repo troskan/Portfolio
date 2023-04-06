6a88ca1b42e6fd81ff50a885e234763803b2ed92
troskan <str4ndb3rg@live.se>
2023-04-06 14:49:13 +0100

    added pictures to portfol-items

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,9 +9,9 @@ function AboutMe() {
           My name is Alvin and I'm 27 years old. I have been working as a
           carpenter for 7 years and have just switched career to programming
           which I totally <strong>love!</strong> <br />
-          <br /> If I were to describe myself I'd say I'm a very happy and
-          positive person, but if you really would like to know more about me
-          dont hesitate to contact me!{" "}
+          <br /> If I were to describe myself I'd say I'm a happy and positive
+          person, but if you really would like to know more about me dont
+          hesitate to contact me!{" "}
           <h2 id="footer-mail">alvin.strandberg@proton.me</h2>
         </p>
         <img id="profile-pic" src={ProfilePic} alt="" />
--- src/components/PortfolioBoxes.jsx
+++ src/components/PortfolioBoxes.jsx
@@ -6,12 +6,12 @@ function PortfolioBoxes() {
       <h2 id="projects">Projects</h2>
       <section className="portfol-container">
         <div className="box-container">
-          <div className="portfol-item">
+          <div className="portfol-item" id="portfol-item-1">
             <h3>Entity Framework</h3>
             <p>
               Using Entity Framework to scaffold and handle data from database.
             </p>
-
+            <div className="portfol-spacing"></div>
             <ul className="portfol-list">
               <li>
                 <a
@@ -34,11 +34,12 @@ function PortfolioBoxes() {
         </div>
 
         <div className="box-container">
-          <div className="portfol-item">
+          <div className="portfol-item" id="portfol-item-2">
             <h3>Bank App</h3>
             <p>
               Simulation of an internet bank in shape of a Console App / C#.
             </p>
+            <div className="portfol-spacing"></div>
             <ul className="portfol-list">
               <li>
                 <a target="_blank" href="https://github.com/5laes/Bank_Orange">
@@ -88,9 +89,10 @@ function PortfolioBoxes() {
           </div>
         </div> */}
         <div className="box-container">
-          <div className="portfol-item">
+          <div className="portfol-item" id="portfol-item-3">
             <h3>Quiz Game</h3>
             <p>Answer the correct questions to gain points and highscore.</p>
+            <div className="portfol-spacing"></div>
             <ul className="portfol-list">
               <li>
                 <a
@@ -112,9 +114,10 @@ function PortfolioBoxes() {
           </div>
         </div>
         <div className="box-container">
-          <div className="portfol-item">
+          <div className="portfol-item" id="portfol-item-4">
             <h3>Portfolio</h3>
             <p>The portfolio you are browsing right now, React + Vite</p>
+            <div className="portfol-spacing"></div>
             <div className="portfol-list-container">
               <ul className="portfol-list">
                 <li>
